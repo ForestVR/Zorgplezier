d8649181d1c77347f1057746ca60857c8f189c97
Geffrey de Winter <geff@nanohost.nl>
2018-09-14 11:27:31 +0200

    trying something else

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -8,21 +8,21 @@ curl -o Unity.pkg https://download.unity3d.com/download_unity/3071d1717b71/MacEd
 echo 'Installing Unity.pkg'
 sudo installer -dumplog -package Unity.pkg -target /
 
-curl -o Unity-Mac-Support.pkg https://download.unity3d.com/download_unity/3071d1717b71/MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-2018.2.5f1.pkg 
+curl -o /Unity-Mac-Support.pkg https://download.unity3d.com/download_unity/3071d1717b71/MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-2018.2.5f1.pkg 
 
 echo 'Installing Unity-Mac-Support.pkg'
-sudo installer -dumplog -package Unity-Mac-Support.pkg 
+sudo installer -dumplog -package Unity-Mac-Support.pkg -target /
 
 
-curl -o Unity-Win-Support.pkg https://download.unity3d.com/download_unity/3071d1717b71/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-2018.2.5f1.pkg
+curl -o /Unity-Win-Support.pkg https://download.unity3d.com/download_unity/3071d1717b71/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-2018.2.5f1.pkg
 
 echo 'Installing Unity-Win-Support.pkg'
-sudo installer -dumplog -package Unity-Win-Support.pkg
+sudo installer -dumplog -package Unity-Win-Support.pkg -target /
 
-curl -o Unity-Linux-Support.pkg https://download.unity3d.com/download_unity/3071d1717b71/MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-2018.2.5f1.pkg
+curl -o /Unity-Linux-Support.pkg https://download.unity3d.com/download_unity/3071d1717b71/MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-2018.2.5f1.pkg
 
 echo 'Installing Unity-Linux-Support.pkg'
-sudo installer -dumplog -package Unity-Linux-Support.pkg
+sudo installer -dumplog -package Unity-Linux-Support.pkg -target /
 
 
 echo 'Install.sh ran successfully'
