d3e5165ce924de71968b25fd367d399f1a8061c5
Geffrey de Winter <geff@nanohost.nl>
2018-09-14 10:09:06 +0200

    install.sh curl fix

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -3,14 +3,15 @@
 # Example install script for Unity3D project. See the entire example: 
 
 
-# This link changes from time to time. I haven't found a reliable hosted 
+ 
  
 from: http://unity3d.com/get-unity/download/archive
 echo 'Downloading from 
-http://netstorage.unity3d.com/unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg: 
-'
-curl -o Unity.pkg 
 http://netstorage.unity3d.com/unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg
+'
+curl -o Unity.pkg http://netstorage.unity3d.com/unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg
 
 echo 'Installing Unity.pkg'
-sudo installer -dumplog -package Unity.pkg -target /
+sudo installer -dumplog -package Unity.pkg -target /x
+
+echo 'Install.sh ran successfully'
