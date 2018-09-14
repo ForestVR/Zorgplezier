6d9fd8c197fef4dac3c961d28f634fef440e6c9a
Geffrey de Winter <geff@nanohost.nl>
2018-09-14 11:05:48 +0200

    changed install path

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,11 +1,5 @@
 #! /bin/sh
 
-# Example install script for Unity3D project. See the entire example: 
-
-
- 
- 
-from: http://unity3d.com/get-unity/download/archive
 echo 'Downloading from 
 https://download.unity3d.com/download_unity/3071d1717b71/MacEditorInstaller/Unity-2018.2.5f1.pkg
 '
@@ -17,18 +11,18 @@ sudo installer -dumplog -package Unity.pkg -target /
 curl -o Unity-Mac-Support.pkg https://download.unity3d.com/download_unity/3071d1717b71/MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-2018.2.5f1.pkg 
 
 echo 'Installing Unity-Mac-Support.pkg'
-sudo installer -dumplog -package Unity-Mac-Support.pkg -target /
+sudo installer -dumplog -package Unity-Mac-Support.pkg 
 
 
 curl -o Unity-Win-Support.pkg https://download.unity3d.com/download_unity/3071d1717b71/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-2018.2.5f1.pkg
 
 echo 'Installing Unity-Win-Support.pkg'
-sudo installer -dumplog -package Unity-Win-Support.pkg -target /
+sudo installer -dumplog -package Unity-Win-Support.pkg
 
 curl -o Unity-Linux-Support.pkg https://download.unity3d.com/download_unity/3071d1717b71/MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-2018.2.5f1.pkg
 
 echo 'Installing Unity-Linux-Support.pkg'
-sudo installer -dumplog -package Unity-Linux-Support.pkg -target /
+sudo installer -dumplog -package Unity-Linux-Support.pkg
 
 
 echo 'Install.sh ran successfully'
