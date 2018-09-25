55523ed04b5cdb0dfb0d27582df5f8bded197928
Geffrey de Winter <geff@nanohost.nl>
2018-09-25 15:13:20 +0200

    removed variables from file and fixed syntax

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -29,7 +29,6 @@ echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME}"
 	-editorTestsResultFile $(pwd)/test.xml \
 	-quit
 
-rc0=$?
 # echo "Unit test logs"
 # cat $(pwd)/test.xml
 # exit if tests failed
@@ -45,11 +44,10 @@ echo "Attempting build of ${UNITYCI_PROJECT_NAME} for Windows"
 	-silent-crashes \
 	-logFile $(pwd)/unity.log \
 	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
-	-buildWindowsPlayer 
+	-buildWindowsPlayer \
 "$(pwd)/Build/windows/${UNITYCI_PROJECT_NAME}.exe" \
 	-quit
 
-rc1=$?
 echo "Build logs (Windows)"
 cat $(pwd)/unity.log
 
@@ -60,12 +58,10 @@ echo "Attempting build of ${UNITYCI_PROJECT_NAME} for OSX"
 	-silent-crashes \
 	-logFile $(pwd)/unity.log \
 	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
-	-buildOSXUniversalPlayer 
+	-buildOSXUniversalPlayer \
 "$(pwd)/Build/osx/${UNITYCI_PROJECT_NAME}.app" \
 	-quit
 
-rc2=$?
 echo "Build logs (OSX)"
 cat $(pwd)/unity.log
 
-exit $(($rc1|$rc2))
